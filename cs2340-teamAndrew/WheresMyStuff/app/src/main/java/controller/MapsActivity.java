79fc7fe5b8e76a0572a7bb6894f813dc05999ee3
Andrew Tuttle <atuttle7@gatech.edu>
2017-07-09 17:50:29 -0400

    Moved Maps File to Controller, Fixed Typo in Found Items Test Box

--- cs2340-teamAndrew/WheresMyStuff/app/src/main/java/controller/MapsActivity.java
+++ cs2340-teamAndrew/WheresMyStuff/app/src/main/java/controller/MapsActivity.java
@@ -1,4 +1,4 @@
-package cs2340teamandrew.wheresmystuff;
+package controller;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import cs2340teamandrew.wheresmystuff.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 

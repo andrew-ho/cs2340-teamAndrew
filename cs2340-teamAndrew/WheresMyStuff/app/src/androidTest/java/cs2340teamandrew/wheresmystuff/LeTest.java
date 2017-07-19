edefe5d34a227447f8bf1f8eb491f0d4d8f9405c
Andrew <andrew7@gatech.edu>
2017-07-18 21:52:05 -0400

    test

--- cs2340-teamAndrew/WheresMyStuff/app/src/androidTest/java/cs2340teamandrew/wheresmystuff/LeTest.java
+++ cs2340-teamAndrew/WheresMyStuff/app/src/androidTest/java/cs2340teamandrew/wheresmystuff/LeTest.java
@@ -1,8 +1,16 @@
 package cs2340teamandrew.wheresmystuff;
 
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
+import controller.LoginScreenActivity;
+
 /**
  * Created by Andrew on 7/18/2017.
  */
 
 public class LeTest {
+    @Rule
+    public ActivityTestRule<LoginScreenActivity> mActivityTestRule = new ActivityTestRule<>(LoginScreenActivity.class);
 }
